3c18d36d09074a3190880cb6b8bf45e032a2dee4
MD. Shimul <mdshemul480@gmail.com>
2022-02-12 12:08:05 +0600

    adding login page code

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,5 +1,17 @@
 @extends('layout')
 
 @section('content')
-    <h1>hello world</h1>
+    <h1>login page</h1>
+    <form action="/loginSubmit" method="post">
+        <div class="form-group">
+            <label for="exampleInputEmail1">Email address</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+                placeholder="Enter email">
+        </div>
+        <div class="form-group">
+            <label for="exa mpleInputPassword1">Password</label>
+            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
 @endsection
