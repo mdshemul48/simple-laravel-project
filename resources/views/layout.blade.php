f347c19124d0549ae50e31619caa2dbffc392f02
MD. Shimul <mdshemul480@gmail.com>
2022-02-12 11:28:34 +0600

    adding nav bar

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -16,8 +16,22 @@
 
     <header class="header">
         @section('header')
-            <h1>header part</h1>
-        @show()
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <a class="navbar-brand" href="#">Navbar</a>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">User List </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Login</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Create account</a>
+                        </li>
+                    </ul>
+                </div>
+        </nav> @show()
     </header>
     <main class="content">
         @section('content')
